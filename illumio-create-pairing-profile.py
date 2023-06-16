f0c39d1a36439dd884f80d6493016d531da776bb
alexgoller <83632450+alexgoller@users.noreply.github.com>
2023-06-16 11:01:55 +0200

    Changes in argument parsing, additional parameters

--- illumio-create-pairing-profile.py
+++ illumio-create-pairing-profile.py
@@ -15,15 +15,16 @@ def write_to_file(keyfile, content):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='PCE Demo Host Credentials')
-    parser.add_argument('--pce_host', help='Integer for the PCE demo host')
-    parser.add_argument('--pce_port', default=443, help='TCP port for the PCE connection')
-    parser.add_argument('--org_id', default=1, help='Organization ID for the PCE')
-    parser.add_argument('--username', default='demo@illumio.com', help='Optional username (default: demo@illumio.com)')
-    parser.add_argument('--password', default='password', help='Optional password (default: password)')
+    parser.add_argument('--pce_host', default=os.environ.get('PCE_HOST', 'poc1.illum.io'), help='Integer for the PCE demo host')
+    parser.add_argument('--pce_port', default=os.environ.get('PCE_PORT', 443), help='TCP port for the PCE connection')
+    parser.add_argument('--org_id', default=os.environ.get('PCE_ORG', 1), help='Organization ID for the PCE')
+    parser.add_argument('--api_user', default=os.environ.get('PCE_API_USER'), help='Optional username (default: demo@illumio.com)')
+    parser.add_argument('--api_key', default=os.environ.get('PCE_API_KEY'), help='Optional password (default: password)')
     parser.add_argument('--labels', help='Optional labels in the form of key-value pairs separated by commas')
     parser.add_argument('--allowed_uses_per_key', default=1, help='Key can be used this often (default: 1))')
     parser.add_argument('--verbose', action='store_true', help='Be more verbose (logging)')
     parser.add_argument('--keyfile-base', default='pairing-key', help='Use this as the basename for the keyfile generated. Rest will be the pairing profile name.')
+    parser.add_argument('--pp_name', default='Cloud-Provisioning-{}'.format(uuid.uuid4().hex[:6].upper()), help='Pairing Profile Name')
     return parser.parse_args()
 
 # Parsing the arguments
@@ -33,13 +34,22 @@ args = parse_arguments()
 pce_host = args.pce_host
 pce_port = args.pce_port
 org_id = args.org_id
-username = args.username
-password = args.password
+username = args.api_user
+password = args.api_key
 labels = args.labels.split(',') if args.labels else []
 verbose = args.verbose
 allowed_uses_per_key = args.allowed_uses_per_key
 keyfile_base = args.keyfile_base
+pp_name = args.pp_name
 
+if not pce_host:
+    exit("PCE Host (--pce_host or environemnt variable PCE_HOST) is required")
+
+if not username:
+    exit("API User (--api_user or environment variable PCE_API_USER) is required")
+
+if not password:
+    exit("API Key (--api_key or environment variable PCE_API_KEY) is required")
 
 if verbose:
     print("Verbose logging enabled")
@@ -54,6 +64,9 @@ logging.debug(f"PCE Port: {pce_port}")
 logging.debug(f"Organization ID: {org_id}")
 logging.debug(f"Username: {username}")
 logging.debug(f"Labels: {labels}")
+logging.debug(f"Allowed uses per key: {allowed_uses_per_key}")
+logging.debug(f"Keyfile base: {keyfile_base}")
+logging.debug(f"Pairing Profile Name: {pp_name}")
 
 pce = PolicyComputeEngine(pce_host, port=pce_port, org_id=org_id)
 pce.set_credentials(username, password)
@@ -85,7 +98,9 @@ for l in labels:
 for item in pairing_labels:
     logging.debug("Item: {} = {}".format(item.key, item.value))
 
-pp_name = "Cloud-Provisioning-{}".format(uuid.uuid4().hex[:6].upper())
+if not pp_name:
+    pp_name = "Cloud-Provisioning-{}".format(uuid.uuid4().hex[:6].upper())
+
 logging.debug("Pairing Profile Name: {}".format(pp_name))
 
 pairing_profile = PairingProfile(
